b1db18103d9ed262a843fe32c0f4c8a292e51021
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-12 23:01:26 +0700

    Style Card Product

--- src/components/Products/BestProductsList.js
+++ src/components/Products/BestProductsList.js
@@ -2,20 +2,21 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const BestProductsList = ({ product }) => {
   return (
-    <div className="col-md-3">
-      <div className="BestProducts__card">
-        <div className="BestProducts__card__img">
+    <div className="col-md-3 p-15 ">
+      <div className="bestProducts__card">
+        <div className="bestProducts__card__img">
           <LazyLoadImage src={product.image} alt={product.name} />
         </div>
-        <div className="BestProducts__card__layer">
-          <div className="BestProducts__card__layer__content">
-            <div className="BestProducts__card__layer__content__name">
+        <div className="bestProducts__card__layer">
+          <div className="bestProducts__card__layer__content">
+            <div className="bestProducts__card__layer__content__name">
               {product.name}
             </div>
+            <div className="bestProducts__card__layer__content__price">
+              <span>$</span>
+              {product.price}
+            </div>
           </div>
-        </div>
-        <div className="BestProducts__card__info">
-          <div className="BestProducts__card__info__text">Explore</div>
         </div>
       </div>
     </div>
