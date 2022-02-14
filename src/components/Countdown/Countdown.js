18b05f3b054bd368ae5f60738a2cb7b624ee2cf5
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-14 17:59:49 +0700

    Countdown time

---
+++ src/components/Countdown/Countdown.js
@@ -0,0 +1,71 @@
+import { useRef, useState, useEffect } from "react";
+
+const Countdown = () => {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countDate = new Date("May 30, 2023 00:00:00").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDate - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
+  return (
+    <div className="countdown__contents">
+      <section>
+        <p>
+          {timerDays} <small>Days</small>
+        </p>
+      </section>
+      <span>:</span>
+      <section>
+        <p>
+          {timerHours} <small>Hours</small>
+        </p>
+      </section>
+      <span>:</span>
+      <section>
+        <p>
+          {timerMinutes} <small>Minutes</small>
+        </p>
+      </section>
+      <span>:</span>
+      <section>
+        <p>
+          {timerSeconds} <small>Seconds</small>
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Countdown;
--- src/components/Products/SaleProducts.js
+++ src/components/Products/SaleProducts.js
@@ -1,27 +1,39 @@
-import { useState,useContext } from "react";
+import { useState, useContext } from "react";
 import SaleProductsContext from "../../context/SaleProductsContext";
+import Countdown from "../Countdown/Countdown";
 import SaleProductsList from "./SaleProductsList";
 
 const SaleProducts = () => {
   const [state] = useState({
     title: "Sales",
   });
-  const {SaleProductsData: {saleProducts}, } = useContext(SaleProductsContext);
-  console.log(saleProducts)
-  
+  const {
+    SaleProductsData: { saleProducts },
+  } = useContext(SaleProductsContext);
+
   return (
     <div className="saleProducts">
       <div className="saleProducts__contents">
         <div className="container">
           <div className="row">
             <div className="col-md-6">
-              <div className="saleProducts__contents__heading">{state.title}</div>
+              <div className="saleProducts__contents__heading">
+                {state.title}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="countdown">
+                <Countdown />
+              </div>
             </div>
           </div>
           <div className="saleProducts__blocks">
             <div className="row">
               {saleProducts.map((saleProduct) => (
-                <SaleProductsList saleProducts={saleProduct} key={saleProduct.id} />
+                <SaleProductsList
+                  saleProducts={saleProduct}
+                  key={saleProduct.id}
+                />
               ))}
             </div>
           </div>
