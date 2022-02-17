325d59db3ae0d074bd5c1df22e908b4876b1c20d
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-17 15:09:07 +0700

    Style Seach Product

--- src/components/PageParts/SearchBar.js
+++ src/components/PageParts/SearchBar.js
@@ -2,12 +2,16 @@ const SearchBar = () => {
   return (
     <>
       <div className="page__content__search">
-        <input className="page__content__search__searchInput" type="text" name=""  />
+        <input
+          className="page__content__search__searchInput"
+          type="text"
+          name=""
+        />
+
         <button className="page__content__search__searchButton" href="#">
           <i className="material-icons">search</i>
         </button>
       </div>
-     
     </>
   );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => {
     pageHeading:
       "We believe in our associates, give back to our communities and put the customer first.",
     message:
-      "We are a diverse community in which all associates are treated fairly and respectfully, with equal access to opportunities and resources, allowing associates to contribute fully to the company’s success. We foster a culture where everyone can bring their whole selves to work. As our world evolves, we evolve with it by embracing social change, taking action and holding ourselves accountable for creating a happy, healthy, and safe place to work.",
+      "We are a diverse community in which all associates are treated fairly and respectfully, with equal access to opportunities and resources, allowing associates to contribute fully to the company’s success. We foster a culture where everyone can bring their whole selves to work. As our world evolves, we evolve with it by embracing social change, taking action and holding ourselves accountable for creating a happy, healthy, and safe place to work. We are a community of smart, passionate, and committed associates whose creativity, innovation and hard work fuel the world’s most recognized lingerie, apparel and beauty brands.",
   });
 
   return (
